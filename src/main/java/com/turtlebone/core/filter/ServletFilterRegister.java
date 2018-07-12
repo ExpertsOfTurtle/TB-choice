2d79a4b5d0699ff0ee02520028dfd657fa2d2f3e
danny01.feng <danny01.feng@vipshop.com>
2018-07-12 13:44:00 +0800

    Choice

---
+++ src/main/java/com/turtlebone/core/filter/ServletFilterRegister.java
@@ -0,0 +1,50 @@
+package com.turtlebone.core.filter;
+
+import javax.servlet.ServletException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import com.turtlebone.choice.service.UserService;
+import com.turtlebone.core.service.RedisService;
+
+@Component
+public class ServletFilterRegister {
+
+	@Autowired
+	private Environment env;
+
+	@Value("${constants.excludeRegex}")
+	private String excludeRegex;
+	@Value("${core.basehost}")
+	private String BASEHOST;
+	
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private RedisService redisService;
+	
+	@Bean
+	public FilterRegistrationBean registrationAuthenticationFilter() throws ServletException {
+		
+		SecurityFilter authenticationFilter = new SecurityFilter();
+//		MockSecurityFilter authenticationFilter = new MockSecurityFilter();
+		
+		authenticationFilter.setEnv(env);
+		authenticationFilter.setUserService(userService);
+		authenticationFilter.setRedisService(redisService);
+		authenticationFilter.setBASEHOST(BASEHOST);
+		
+		FilterRegistrationBean registration = new FilterRegistrationBean(authenticationFilter);
+		registration.addUrlPatterns("/*");
+		registration.addInitParameter("excludeRegex", excludeRegex);
+		registration.setOrder(0);
+
+		return registration;
+	}
+
+	
+}
